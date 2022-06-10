50458ac96ed70cbf9a963bd477a27185f50457f5
kavpandh <107238835+kavpandh@users.noreply.github.com>
2022-06-10 13:54:51 +0530

    Add files via upload

---
+++ hashTable.java
@@ -0,0 +1,14 @@
+package Demo_package;
+
+public class hashTable {
+
+	public static void main(String arg[]) {
+		int sum =10;
+		for(int i=-1;i<5;++i) {
+			sum =sum/i;
+			System.out.println(sum);
+		
+		
+	}}
+}
+
